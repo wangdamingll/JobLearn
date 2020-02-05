8350370749d61c875434e592dfb3e43fd7231bc6
unknown <wangdamingll@163.com>
2020-02-05 09:55:04 +0800

    add 美团

--- interview/2019/code/HashMap1.cpp
+++ interview/2019/code/HashMap1.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 //hash table 内部节点
-template <typename T>
+template <typename T,typename U>
 class Node{
 public:
-    Node(const string& key,const T value){
+    Node(const T& key,const T& value){
         m_key = key;
         m_value = value;
     }
@@ -17,32 +17,32 @@ public:
         std::cout<<"~Node()"<<std::endl;
     }
 public:
-    string m_key="";
-    T m_value;
-    std::unique_ptr<Node<T>> pNode = nullptr; //内存是否能回收？
+    T m_key;
+    U m_value;
+    std::unique_ptr<Node<T,U>> pNode = nullptr; //内存是否能回收？
 };
 
 //基于hash table 实现的HashMap 冲突解决使用拉链法
-template <typename T>
+template <typename T,typename U>
 class HashMap{
 public:
     explicit HashMap(const unsigned int size){
         this->m_size = size;
-        m_ppNode = std::make_unique<unique_ptr<Node<T>>[]>(m_size);
+        m_ppNode = std::make_unique<unique_ptr<Node<T,U>>[]>(m_size);
         for(unsigned int index=0;index<m_size;index++){
             m_ppNode[index].reset(nullptr);
         }
     }
 public:
-    void Insert(const string&key,const T value){
-        int index = std::hash<string>{}(key)%m_size; //std::hash C++ 17
-        std::unique_ptr<Node<T>> newNode = std::make_unique<Node<T>>(key,value);
+    void Insert(const T& key,const U& value){
+        int index = std::hash<T>{}(key)%m_size; //std::hash C++ 17
+        std::unique_ptr<Node<T,U>> newNode = std::make_unique<Node<T,U>>(key,value);
         if(m_ppNode[index] == nullptr){
-            m_ppNode[index] = std::move(newNode);
+            m_ppNode[index] = std::move(newNode);//内存是否被正确回收？待验证
             return ;
         }
         //map容器考虑去重
-        Node<T>* node = m_ppNode[index].get();
+        Node<T,U>* node = m_ppNode[index].get();
         while(node->pNode!= nullptr){
             if(node->m_key == key){
                 node->m_value = value;
@@ -57,8 +57,8 @@ public:
         //没有重复的元素 采用尾插法
         node->pNode = std::move(newNode);
     }
-    T Get(const string&key){
-        int index = std::hash<string>{}(key)%m_size;
+    U Get(const T&key){
+        int index = std::hash<T>{}(key)%m_size;
         auto p = m_ppNode[index].get();
         while(p!= nullptr){
             if(p->m_key == key){
@@ -66,10 +66,10 @@ public:
             }
             p = p->pNode.get();
         }
-        return T();
+        return U();
     }
-    bool Find(const string&key){
-        int index = std::hash<string>{}(key)%m_size;
+    bool Find(const T&key){
+        int index = std::hash<T>{}(key)%m_size;
         auto p = m_ppNode[index].get();
         while(p!= nullptr){
             if(p->m_key == key){
@@ -80,13 +80,13 @@ public:
         return false;
     }
 private:
-    std::unique_ptr<unique_ptr<Node<T>>[]> m_ppNode = nullptr;//unique_ptr对象数组
+    std::unique_ptr<unique_ptr<Node<T,U>>[]> m_ppNode = nullptr;//unique_ptr对象数组
     unsigned int m_size=0;
 };
 
 int main() {
     {
-        HashMap<string> map(100);
+        HashMap<string,string> map(100);
         map.Insert("a","aaa");
         map.Insert("b","bbb");
 
