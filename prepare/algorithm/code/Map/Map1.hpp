65781de795100421a43e9c0a27cbfdfac4399554
wangdamingll@163.com <wangdamingll@163.com>
2020-04-07 12:49:34 +0800

    add map algorithm

---
+++ prepare/algorithm/code/Map/Map1.hpp
@@ -0,0 +1,47 @@
+#ifndef __MAP1__H__
+#define __MAP1__H__
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+/*图遍历----深度优先搜索算法(DFS)-----无向图
+ *
+ * 问题描述:
+ *
+ *
+ * 思考:
+ *
+ * */
+
+
+
+void PrintMap4(){
+
+}
+
+
+void CreateMap1(){
+
+}
+
+
+void DFS1(int x,int y,int color){
+
+}
+
+
+int TestDFS1(){
+    auto start = std::chrono::steady_clock::now();
+
+
+
+    auto end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    std::cout<<"time:"<<time.count()<<"ms"<<std::endl;
+
+    return 0;
+}
+
+
+#endif //__MAP1__H__
---
+++ prepare/algorithm/code/Map/main.cpp
@@ -0,0 +1,19 @@
+#include "Map1.hpp"
+using namespace std;
+
+
+/* 算法:图遍历----深度优先搜索算法(DFS)-----无向图和有向图
+ *
+ * 算法思想:
+ *
+ *
+ * 注意事项:
+ *
+ *
+ * */
+
+
+
+int main() {
+    TestDFS1();         //图遍历-DFS-有向图
+}
