cbc602b3927edb74c7261e5b7f098187cf5ccbfc
wangdamingll@163.com <wangdamingll@163.com>
2020-04-08 17:45:34 +0800

    add Floyd-Warshall

--- prepare/algorithm/code/Floyd-Warshall/main.cpp
+++ prepare/algorithm/code/Floyd-Warshall/main.cpp
@@ -1,6 +1,4 @@
 #include "Map1.hpp"
-#include "Map2.hpp"
-#include "Map3.hpp"
 using namespace std;
 
 
@@ -9,11 +7,11 @@ using namespace std;
  * 图定义：
  * 简单的说,图就是由一些小圆点(称为顶点)和连接这些小圆点的直线(称为边)组成的.
  *
+ * Floyd-Warshall(弗洛伊德算法)
+ * 解决多源最短路问题(也可以是单源最短路)
  * */
 
 
 int main() {
-    TestDFS1();         //图遍历-DFS-无向图
-    TestBFS2();         //图遍历-BFS-无向图
-    TestDFS3();         //图遍历-DFS-有向图
+    TestFWS1();         //Floyd-Warshall
 }
