6db17e3eec0b18c16671e9eb92ec1cb5a0e9dbc9
wangdamingll@163.com <wangdamingll@163.com>
2020-04-10 15:28:27 +0800

    add Dijkstra

--- prepare/algorithm/code/Dijkstra/main.cpp
+++ prepare/algorithm/code/Dijkstra/main.cpp
@@ -1,5 +1,6 @@
 #include "Dijkstra1.hpp"
 #include "Dijkstra2.hpp"
+#include "Dijkstra3.hpp"
 using namespace std;
 
 
@@ -15,5 +16,5 @@ using namespace std;
 int main() {
     TestMapStore();         //图存储----邻接表存储法
     TestDijkstra2();        //Dijkstra--单源最短路---邻接矩阵存储法
-    //TestDijkstra3();         //Dijkstra--单源最短路---邻接表存储法(对于稀疏图的优化)
+    TestDijkstra3();         //Dijkstra--单源最短路---邻接表存储法(对于稀疏图的优化)
 }
