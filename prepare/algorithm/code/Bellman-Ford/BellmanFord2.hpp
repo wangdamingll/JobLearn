c25132608c0d8928cd97eefcb40034e4b9e64f84
wangdamingll@163.com <wangdamingll@163.com>
2020-04-13 15:55:55 +0800

    add Bellman-Ford

--- prepare/algorithm/code/Bellman-Ford/BellmanFord2.hpp
+++ prepare/algorithm/code/Bellman-Ford/BellmanFord2.hpp
@@ -15,16 +15,12 @@ using namespace std;
  * 1. 有向图的概念
  * 边有方向的图称为有向图
  * 2. 第一行两个整数n m,n表示顶点个数(顶点编号为1~n),m表示边的条数. 接下来m行,每行有3个数x y z,表示顶点x到顶点y的边的权值为z
- *  6 9
- *  1 2 1
- *  1 3 12
- *  2 3 9
- *  2 4 3
- *  3 5 5
- *  4 3 4
- *  4 5 13
- *  4 6 15
- *  5 6 4
+ *  5 5
+ *  2 3 2
+ *  1 2 -3
+ *  1 5 5
+ *  4 5 2
+ *  3 4 3
  *
  * 算法思想:
  * 一句话概括:对所有的边进行n-1次"松弛"操作(n为顶点数量)(因为在一个含有n个顶点的图中,任意两点之间的最短路径最多包含n-1边)
@@ -47,20 +43,20 @@ using namespace std;
  * */
 
 //为方便这里就直接把上图的行数据初始化了
-constexpr int n2=6; //图的顶点数量(上面的数据)
-constexpr int m2=9; //图的边数量(上面的数据)
+constexpr int n2=5; //图的顶点数量(上面的数据)
+constexpr int m2=5; //图的边数量(上面的数据)
 
 //比m大1 分别存储顶点i到顶点j的权值,这里直接初始化上图数据
-static int u2[m2+1]={0,1,1, 2,2,3,4,4, 4, 5};//顶点i
-static int v2[m2+1]={0,2,3, 3,4,5,3,5, 6, 6};//顶点j
-static int w2[m2+1]={0,1,12,9,3,5,4,13,15,4};//权值
+static int u2[m2+1]={0,2,1, 1,4,3};//顶点i
+static int v2[m2+1]={0,3,2, 5,5,4};//顶点j
+static int w2[m2+1]={0,2,-3,5,2,3};//权值
 
 //比n大1 分别存储编号i的第一条边的编号和编号i的边的下一条边的编号
 int first2[n2+1]={0};
 //比m大1
 int next2[m2+1]={0};
 
-int visit2[n2+1]={0};//标识顶点是否最短距离已知
+//int visit2[n2+1]={0};//表示顶点是否在队列中
 
 //邻接表存储
 void StoreMap2(){
@@ -88,16 +84,27 @@ void PrintMap2(){
 }
 
 //未优化
-void BellmanFord2(){
+void BellmanFord21(){
     StoreMap2();//存储图
     PrintMap2();//打印图
 
     //Dijkstra算法 这里以1号顶点为源点s,顶点s不能直接到达的依旧用99999999表示
     //初始化dis
-    int dis[n2+1]={99999999,0,99999999,99999999,99999999,99999999,99999999};
+    int dis[n2+1]={99999999,0,99999999,99999999,99999999,99999999};
+    int k = first2[1];
+    while(k!=-1){
+        dis[v2[k]] = w2[k];
+        k = next2[k];
+    }
 
-    //待完善
-
+    //Bellman-Ford核心代码:对所有边进行n-1次松弛
+    for(int i=1;i<=n2-1;i++){ //最多n-1次松弛
+        for(int j=1;j<=m2;j++){ //所有边
+            if(dis[v2[j]]>dis[u2[j]]+w2[j]){
+                dis[v2[j]]=dis[u2[j]]+w2[j];
+            }
+        }
+    }
 
     //输出最终结果
     std::cout<<"顶点1能够到达其他顶点的距离为:"<<std::endl;
@@ -107,11 +114,61 @@ void BellmanFord2(){
     std::cout<<std::endl;
 }
 
+//优化1:如果第k次松弛后,dis和结果和第K-1次一样,那就退出循环,因为目的以达到
+void BellmanFord22(){
+    StoreMap2();//存储图
+    PrintMap2();//打印图
+
+    //Dijkstra算法 这里以1号顶点为源点s,顶点s不能直接到达的依旧用99999999表示
+    //初始化dis
+    int dis[n2+1]={99999999,0,99999999,99999999,99999999,99999999};
+    int bak[n2+1]={0};//每次松弛备份dis做比较
+    int k = first2[1];
+    while(k!=-1){
+        dis[v2[k]] = w2[k];
+        k = next2[k];
+    }
+
+    //Bellman-Ford核心代码:对所有边进行n-1次松弛
+    for(int i=1;i<=n2-1;i++){ //最多n-1次松弛
+        for(int k2=1;k2<=n2;k2++){//备份dis
+            bak[k2] = dis[k2];
+        }
+        for(int j=1;j<=m2;j++){ //所有边
+            if(dis[v2[j]]>dis[u2[j]]+w2[j]){
+                dis[v2[j]]=dis[u2[j]]+w2[j];
+            }
+        }
+
+        //优化
+        int check=0;
+        for(int tmp=1;tmp<=n2;tmp++){
+            if(bak[tmp]!=dis[tmp]){
+                check=1;
+                break;
+            }
+        }
+        if(check==0){ //如果dis数组没有更新,则退出循环
+            break;
+        }
+    }
+
+    //输出最终结果
+    std::cout<<"顶点1能够到达其他顶点的距离为:"<<std::endl;
+    for(int i=1;i<=n2;i++){
+        std::cout<<"1->"<<i<<":"<<dis[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+
 //单源最短路
 int TestBellmanFord2(){
     auto start = std::chrono::steady_clock::now();
 
-    BellmanFord2();//未优化
+    BellmanFord21();//未优化
+
+    BellmanFord22();//如果第k次松弛后,dis和结果和第K-1次一样,那就退出循环,因为目的以达到
 
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
