49f94d611ca32e78da4c8d6d3bf3dd3727722a37
wangdamingll@163.com <wangdamingll@163.com>
2020-04-15 18:08:20 +0800

    add HeapSort

--- prepare/algorithm/code/HeapSort/HeapSort1.hpp
+++ prepare/algorithm/code/HeapSort/HeapSort1.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <chrono>
+#include <algorithm>
+#include <utility>
 using namespace std;
 
 
@@ -21,13 +23,95 @@ using namespace std;
  *
  * */
 
+constexpr int n1 = 8;
+int h1[n1+1] ={0,3,8,9,6,5,4,2,1};
 
+void Print(){
+    for(int i=1;i<=n1;i++){
+        std::cout<<h1[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+//交换
+void Swap(int x,int y){
+#if 1
+    std::swap(h1[x],h1[y]);
+#else
+    int t = h1[x];
+    h1[x] = h1[y];
+    h1[y] = t;
+#endif
+}
+
+//向下调整:往往在更新h[1]即第一个元素时,向下调整
+void SiftDown(int i,int len){
+    int flag =0;//需不需要继续调整
+    int t =0; //记录最大值的编号
+    while(i*2<=len&&flag==0){ //表示i节点有儿子(如有儿子必须是左子树),且需要调整
+        t = i;//假设当前节点值最大
+        if(h1[i]<h1[2*i]){//要求升序排序,就需要这里以最大堆为例
+            t = 2*i;
+        }
+
+        if(2*i+1<=len && h1[t]<h1[2*i+1]){//存在右子树
+            t = 2*i+1;
+        }
+
+        if(t!=i){//表示最大值有变动
+            Swap(i,t);//交换i 和 t(最大值编号)
+            i = t;
+        } else{
+            flag=1;
+        }
+    }
+}
+
+//向上调整:向堆中插入新元素时(实际上是插入到末尾),需要向上调整
+void SiftUp(int i){
+    int flag =0;
+    if(i==1){   //是根顶点
+        return ;
+    }
+
+    while(i!=1 && flag==0){ //i不是根顶点且需要在调整
+        if(h1[i/2]<h1[i]){ //这里以最大堆为例
+            Swap(i/2,i);
+        } else{
+            flag = 1;
+        }
+        i = i/2; //更新编号
+    }
+
+}
+
+//创建堆
+void CreatHeap(){
+    for(int i=n1/2;i>=1;i--){//从最后一个非叶子节点开始调整,依次向上
+        SiftDown(i,n1);     //n1为整个堆的大小,从最后一个非叶子节点开始,向下调整
+    }
+}
+
+//堆排序
+void HeapSort1(){
+    Print();
+    //创建堆
+    CreatHeap();
+
+    //堆排序
+    int i=n1;
+    while(i>1){
+        Swap(1,i);       //交换元素
+        SiftDown(1,--i); //注意--i,将最大值交换到末尾,堆的调整大小减1,调整前面的堆(从根向下调整)
+    }
+
+    Print();
+}
 
 int TestHeapSort1(){
     auto start = std::chrono::steady_clock::now();
 
-
-
+    HeapSort1();//堆排序
 
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
