c577f3ad67e21934c408d4b730dac045747cd263
wangdaming <wangdamingll@163.com>
2020-05-04 09:21:40 +0800

    add TSP

--- prepare/algorithm/code/TSP/TSP.hpp
+++ prepare/algorithm/code/TSP/TSP.hpp
@@ -47,15 +47,23 @@ using namespace std;
  * 1. 旅行商问题是有向图模型,且A到B和B到A极有可能权不一样,这里为方便起见,以权相同处理.
  * */
 
-struct Info3{
-    Info3() = default;
-    Info3(int index,int dis):m_index(index),m_dis(dis){};
+struct Edge{
+    Edge()= default;
+    Edge(int u1,int v1,int w1):u(u1),v(v1),w(w1){}
+    int u {0};//出边点
+    int v {0};//终边点
+    int w {0};//权值
+};
 
-    bool operator <(const Info3& another)const{
+struct HeapInfo{
+    HeapInfo() = default;
+    HeapInfo(int index,int dis):m_index(index),m_dis(dis){};
+
+    bool operator <(const HeapInfo& another)const{
         return m_dis<another.m_dis;
     }
 
-    bool operator >(const Info3& another)const{
+    bool operator >(const HeapInfo& another)const{
         return m_dis>another.m_dis;
     }
 
@@ -63,44 +71,37 @@ struct Info3{
     int m_dis  {0};//顶点距离
 };
 
-//为方便这里就直接把上图的行数据初始化了
-constexpr int n3=6; //图的顶点数量(上面的数据)
-constexpr int m3=9; //图的边数量(上面的数据)
+constexpr int n=5; //图的顶点数量(上面的数据)
 
-//比m大1 分别存储顶点i到顶点j的权值,这里直接初始化上图数据
-static int u3[m3+1]={0,1,1, 2,2,3,4,4, 4, 5};//顶点i
-static int v3[m3+1]={0,2,3, 3,4,5,3,5, 6, 6};//顶点j
-static int w3[m3+1]={0,1,12,9,3,5,4,13,15,4};//权值
+int visit[n+1]={0};//标识顶点是否最短距离已知
+std::vector<HeapInfo> disV;//堆优化使用
 
-//比n大1 分别存储编号i的第一条边的编号和编号i的边的下一条边的编号
-int first3[n3+1]={0};
-//比m大1
-int next3[m3+1]={0};
-
-int visit3[n3+1]={0};//标识顶点是否最短距离已知
-std::vector<Info3> disV3;//堆优化使用
+std::vector<Edge> adj[n+1];//邻接表存储数据
+std::vector<int> path[n+1];//记录路线
 
 //邻接表存储
-void StoreMap3(){
-    //初始化first3数组
-    for(auto& it : first3){
-        it = -1;
-    }
-
-    //邻接表存储
-    for(int i=1;i<=m3;i++){
-        next3[i] = first3[u3[i]];
-        first3[u3[i]] = i;
-    }
+void StoreMap(){
+    adj[1].emplace_back(Edge {1,2,1});
+    adj[1].emplace_back(Edge {1,3,12});
+    adj[2].emplace_back(Edge {2,3,9});
+    adj[2].emplace_back(Edge {2,4,3});
+    adj[3].emplace_back(Edge {3,5,5});
+    adj[4].emplace_back(Edge {4,3,4});
+    adj[4].emplace_back(Edge {4,5,13});
+    adj[2].emplace_back(Edge {2,1,1});
+    adj[3].emplace_back(Edge {3,1,12});
+    adj[3].emplace_back(Edge {3,2,9});
+    adj[4].emplace_back(Edge {4,2,3});
+    adj[5].emplace_back(Edge {5,3,5});
+    adj[3].emplace_back(Edge {3,4,4});
+    adj[5].emplace_back(Edge {5,4,13});
 }
 
 //遍历邻接表的每一条边
-void PrintMap3(){
-    for(int i=1;i<=n3;i++){
-        int k = first3[i]; //取出顶点
-        while(k!=-1){
-            std::cout<<u3[k]<<" "<<v3[k]<<" "<<w3[k]<<std::endl;
-            k=next3[k];
+void PrintMap(){
+    for(auto it:adj){
+        for(auto iter:it){
+            std::cout<<iter.u<<" "<<iter.v<<" "<<iter.w<<std::endl;
         }
     }
 }
@@ -108,15 +109,15 @@ void PrintMap3(){
 
 //dis中获取最小值堆优化
 //或者使用std::priority_queue 优先级队列优化
-int GetMinElem3(){
+int GetMinElem(){
     int index=0;
     int finish=false;
     do{
-        std::pop_heap(disV3.begin(),disV3.end(),std::greater<Info3>{});//将最小元素移动到末尾
-        auto elem = disV3.back();
-        disV3.pop_back();
+        std::pop_heap(disV.begin(),disV.end(),std::greater<HeapInfo>{});//将最小元素移动到末尾
+        auto elem = disV.back();
+        disV.pop_back();
 
-        if(visit3[elem.m_index]==0){//该顶点没有被处理过
+        if(visit[elem.m_index]==0){//该顶点没有被处理过
             index = elem.m_index;
             finish = true;
         }
@@ -124,48 +125,52 @@ int GetMinElem3(){
     return index;
 }
 
-//邻接接表存储---堆优化
+//NPC问题----旅行商问题
 void TSP(){
-    StoreMap3();//存储图
-    PrintMap3();//打印图
+    StoreMap();//存储图
+    PrintMap();//打印图
 
+    //旅行商问题,这里采用Dijskstra算法作为近似算法求解,选择1号顶为起点
     //Dijkstra算法 这里以1号顶点为源点s,顶点s不能直接到达的依旧用99999999表示
     //初始化dis
-    int dis[n3+1]={99999999,0,99999999,99999999,99999999,99999999,99999999};
-    int k = first3[1];//取出1号顶点所在的行数(也就是上面源数据的1号顶点所在的行)
-    while(k!=-1){
-        dis[v3[k]]=w3[k];//取出1号顶点到顶点k的距离
-        disV3.emplace_back(v3[k],w3[k]);
-        k=next3[k];
+    int dis[n+1]={99999999,0,99999999,99999999,99999999,99999999};
+    for(auto it : adj[1]){
+        dis[it.v] = it.w;
+        disV.emplace_back(HeapInfo{it.v,it.w});//堆
+    }
+    for(int i=1;i<=n;i++){
+        path[i].emplace_back(1);//初始化路劲起点
     }
 
-    std::make_heap(disV3.begin(),disV3.end(),std::greater<Info3>{});//初始化 min heap
+    std::make_heap(disV.begin(),disV.end(),std::greater<HeapInfo>{});//初始化 min heap
 
-    visit3[1] = 1; //表示顶点1的最短距离已知
+    visit[1] = 1; //表示顶点1的最短距离已知
 
     int u =0;
-    for(int i=1;i<=n3-1;i++){//松弛dis中的所有顶点
+    for(int i=1;i<=n-1;i++){//松弛dis中的所有顶点
         //找到dis中距离顶点1最短距离的顶点编号
-        u=GetMinElem3();
+        u=GetMinElem();
 
-        visit3[u] = 1;//标记
-
-        //对u的每条出边进行处理
-        int k1 = first3[u];//取出u号顶点所在的行数(也就是上面源数据的u号顶点所在的行)
-        while(k1!=-1){
-            if(dis[v3[k1]]>dis[u] + w3[k1]){
-                dis[v3[k1]]=dis[u] + w3[k1];
-                disV3.emplace_back(v3[k1],dis[v3[k1]]);
-                std::push_heap(disV3.begin(),disV3.end(),std::greater<Info3>{});
+        //松弛所有u的出边
+        for(auto it:adj[u]){//邻接表存储
+            if(dis[it.v]>dis[u]+it.w){
+                dis[it.v] = dis[u]+it.w;//更新dis
+                disV.emplace_back(HeapInfo{it.v,dis[it.v]});//进行堆排序
+                std::push_heap(disV.begin(),disV.end(),std::greater<HeapInfo>{});
+                path[it.v].emplace_back(it.u);//添加路径
             }
-            k1=next3[k1];
         }
     }
 
     //输出最终结果
     std::cout<<"顶点1能够到达其他顶点的距离为:"<<std::endl;
-    for(int i=1;i<=n3;i++){
-        std::cout<<"1->"<<i<<":"<<dis[i]<<" ";
+    for(int i=1;i<=n;i++){
+        std::cout<<"1->"<<i<<":"<<dis[i]<<std::endl;
+        std::cout<<"路线:";
+        for(auto it:path[i]){
+            std::cout<<it<<"->";
+        }
+        std::cout<<i<<std::endl;
     }
     std::cout<<std::endl;
 }
