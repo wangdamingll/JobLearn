2783348c113a915e5b2b07914ebd2fddd08f35ea
wangdamingll@163.com <wangdamingll@163.com>
2023-04-03 16:24:48 +0800

    update

--- prepare/algorithm/code/QuickSort/QuickSort1.hpp
+++ prepare/algorithm/code/QuickSort/QuickSort1.hpp
@@ -2,82 +2,64 @@
 #define __QUICK_SORT1__H__
 
 #include <iostream>
-#include <chrono>
+#include <string>
+#include <vector>
 #include <algorithm>
-#include <utility>
+
 using namespace std;
 
-/*快速排序----对整个数据进行升序排序
- *
- * 问题描述:
- * 对整个数据进行升序排序:3 8 9 6 5 4 2 1
- *
- * */
+int32_t Division(std::vector<int32_t>& vec, int32_t left, int32_t right)
+{
+    int32_t base = vec[left];//保存基准值
+    while(left < right)
+    {
+        //从右向左找到小于基准值的位置
+        while(left < right && vec[right] >= base)
+        {
+            right--;
+        }
+        vec[left] = vec[right];
 
-constexpr int n1 = 8;
-int h1[n1+1] ={0,3,8,9,6,5,4,2,1};
-
-void Print(){
-    for(int i=1;i<=n1;i++){
-        std::cout<<h1[i]<<" ";
+        //从左向右找大于base值的位置
+        while (left < right && vec[left] <= base)
+        {
+            left++;
+        }
+        vec[right] = vec[left];
     }
-    std::cout<<std::endl;
+    vec[left] = base;//填充基准值
+    return  left;
 }
 
-//交换
-void Swap(int x,int y){
-#if 1
-    std::swap(h1[x],h1[y]);
-#else
-    int t = h1[x];
-    h1[x] = h1[y];
-    h1[y] = t;
-#endif
+void QuickSort(std::vector<int32_t>& vec, int32_t left, int32_t right)
+{
+    if(left < right)
+    {
+        int32_t base = Division(vec, left, right);//找到基准位置
+        QuickSort(vec, left, base - 1);
+        QuickSort(vec, base + 1, right);
+    }
 }
 
+int main()
+{
+    std::vector<int32_t> vec{6, 4, 8, 9, 2, 3, 1};
 
-//快排
-void QuickSort(int low,int hight){
-    if(low>=hight){//递归退出条件
-        return ;
-    }
+    std::for_each(vec.begin(), vec.end(), [](int32_t it)->int {
+        std::cout<<it<<",";
+        return 0;
+    });
+    std::cout <<std::endl;
+    int32_t size = vec.size();
 
-    int i =low;     //哨兵i
-    int j = hight;  //哨兵j
-    int tmp = h1[i];//缓存基准数
+    //快排
+    QuickSort(vec, 0, size - 1);
 
-    while(i!=j){    //当哨兵不相遇时
-        while (h1[j]>=tmp && i<j){//向左找到第一个小于基准数的
-            j--;
-        }
-        while(h1[i]<=tmp && i<j){//向右找到第一个大于基准数的
-            i++;
-        }
-        if(i<j){//哨兵不相遇
-            Swap(i,j);//交换
-        }
-    }
-
-    //当哨兵相遇时,将i和基准数交换
-    Swap(low,i);
-
-    QuickSort(low,i-1);
-    QuickSort(i+1,hight);
-
-}
-
-int TestQuickSort1(){
-    auto start = std::chrono::steady_clock::now();
-    Print();
-
-    QuickSort(1,n1);//快速排序
-
-    Print();
-
-    auto end = std::chrono::steady_clock::now();
-    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-    std::cout<<"time:"<<time.count()<<"ms"<<std::endl;
-
+    std::for_each(vec.begin(), vec.end(), [](int32_t it)->int {
+        std::cout<<it<<",";
+        return 0;
+    });
+    std::cout <<std::endl;
     return 0;
 }
 
